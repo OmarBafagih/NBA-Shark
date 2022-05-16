8f8f8ecab9371edebfee3d925c08c27675ddb051
Omar Bafagih <bafagihomar260@gmail.com>
2022-05-15 23:57:12 -0400

    Removed node modules and added call to API I made

--- main_page.js
+++ main_page.js
@@ -9,7 +9,20 @@ window.onload = function() {
 function getLiveGames(){
     let container = document.getElementById("liveGames");
     let game = document.createElement("li");
-    
+
+    req = new XMLHttpRequest();
+	req.onreadystatechange = function() {
+		if(this.status==200){
+            game.textContent = this.responseText;
+			console.log(this.responseText);
+			
+	    }
+    }
+					
+	req.open("GET", `https://young-basin-92734.herokuapp.com/livegames`);
+	req.setRequestHeader("Content-Type", "application/json");
+	req.send();
+
     
     chrome.runtime.sendMessage({popupOpen: true}, function(response){
         //game.textContent = response.liveGames;
